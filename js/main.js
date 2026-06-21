cea93c3036d65246fa659318e6c4aa6e993c9947
carevalo9763 <christianarevalocavani@gmail.com>
2026-06-20 20:45:28 -0700

    udpated mobile

--- js/main.js
+++ js/main.js
@@ -18,8 +18,12 @@ if (canvas) {
   const ctx = canvas.getContext('2d');
 
   const setCanvasSize = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    const dpr = window.devicePixelRatio || 1;
+    canvas.width = window.innerWidth * dpr;
+    canvas.height = window.innerHeight * dpr;
+    canvas.style.width = window.innerWidth + 'px';
+    canvas.style.height = window.innerHeight + 'px';
+    ctx.scale(dpr, dpr);
   };
   setCanvasSize();
   window.addEventListener('resize', () => {
